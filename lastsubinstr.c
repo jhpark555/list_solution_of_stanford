2f950578083cb16d1e39692f1884e275ef6b0c52
jaehong park <jhpark555@gmail.com>
2021-06-02 10:59:36 -0700

    words in string

---
+++ lastsubinstr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[]="I love programming. I love Codeforwin.";
+    int i;
+    char toFind[]="love";
+    int size=sizeof(str)/sizeof(str[0]);
+    int j=0,count=0,k;
+    int wordsize=sizeof(toFind)/sizeof(toFind[0])-1;
+
+
+        for(i=0; str[i]!='\0'; i++)
+        {
+            if(str[i]==toFind[0]){
+                //printf("@%d",i);
+                for(j=0,k=i; toFind[j]!='\0';j++,k++)
+                 if( toFind[j]==str[k]) count++;
+
+                 if(count==wordsize){
+                     count=0;
+                     printf("Yes:%dth\n",i);
+                 }
+            }
+        }
+
+   //  if(wordsize==count)
+  //  printf("yes %d ",count/wordsize);
+   // else printf("No not here \n");
+
+
+    return 0;
+}
