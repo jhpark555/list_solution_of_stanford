9ecb0b5ac90146c241a2e1ede3febc4739ce0247
jaehong park <jhpark555@gmail.com>
2021-06-16 12:06:43 -0700

    update

--- test.c
+++ test.c
@@ -4,33 +4,17 @@
 
 #define MIN(a,b) ( (a> b )?  b:a)
 #define MAX(a,b) ((a>b)? a:b )
+#define INPUT 7
 
-void decimation(float arr[],int n)
+void decimation2(float *arr,float **arr2,int size,int M)
 {
-  int i,k;
-  for(i=0;i<n;i++)
-  {
-    if(i%2 ==1)
-        arr[i]=arr[i+1];
-  }
-
-  for(i=0;i<n-1;i++)
-  {
-    for(k=i+1;k<n;k++)  arr[k]=arr[k+1];
-  }
-
-}
-
-float *decimation2(float arr[],int size,int M)
-{
-    float *arr2=(float*)calloc(1,sizeof(float));
     int i,j=0;
+    float *ptr=*arr2;
 
     for (i = 0; i<size; i=i+M) {
-      arr2[j++] = arr[i];
-    }
+    ptr[j++] = arr[i];
 
-  return arr2;
+    }
 }
 
 float* convolve(float h[], float x[], int lenH, int lenX, int* lenY)
@@ -66,23 +50,26 @@ float h[]= {-0.01452123, -0.0155227 ,  0.01667252,  0.01800633, -0.01957209,
         0.02369253, -0.0214361 , -0.01957209,  0.01800633,  0.01667252,
        -0.0155227 , -0.01452123};
 
-float x[]={1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0};
+float x[INPUT];
 int i;
 int lenY;
 int M=2;
-for(i=0;i<32;i++) x[i]=i;
+for(i=1;i<=INPUT;i++){
+    x[i-1]=i;
+  }
 
 int sizeH= sizeof(h)/sizeof(h[0]);
 int sizeX= sizeof(x)/sizeof(x[0]);
 
-
-int Size= sizeof(x)/sizeof(x[0]);
-   float *y= convolve(h,x,sizeH,sizeX, &lenY );
+float *y= convolve(h,x,sizeH,sizeX, &lenY );
+float *arr2=(float*)malloc(INPUT*sizeof(float));
 
 //decimation(y,lenY);
-float *arr2 = decimation2(y,lenY,M);
+decimation2(y,&arr2,lenY,M);
 
 for(i=0;i<lenY/M;i++) printf("%f ",arr2[i]);
+
+free(arr2);
 
 return 0;
 }
