a05e8666d6b0cd6b647174f15f62ba444b8c711b
jaehong park <jhpark555@gmail.com>
2021-06-17 18:34:24 -0700

    update list name that can be understandable

--- movenode.c
+++ movenode.c
@@ -39,25 +39,51 @@ void display(struct node *head)
 
 }
 
-void movenode(struct node **dst, struct node **src)
-{
-    struct node *New=*src;
+/* UTILITY FUNCTIONS */
+/* MoveNode() function takes the node from the front of the
+   source, and move it to the front of the dest.
+   It is an error to call this with the source list empty.
 
-    *src=New->next;
-    New->next=*dst;
-    *dst=New;
-}
+   Before calling MoveNode():
+   source == {1, 2, 3}
+   dest == {1, 2, 3}
+
+   Affter calling MoveNode():
+   source == {2, 3}
+   dest == {1, 1, 2, 3} */
+   // Function takes the node from the front of the source and moves it
+   // to the front of the destination
+
+   // new를 src로 만들고,
+   // src는 다음으로 진행하고,
+   //new의 next는 dst가 되고,
+   //dst가 new의 header가 되고..
+
+   void movenode(struct node** destRef, struct node** sourceRef)
+   {
+       // if the source list empty, do nothing
+       if (*sourceRef == NULL) {
+           return;
+       }
+
+       struct node* newNode = *sourceRef;  // the front source node
+       *sourceRef = (*sourceRef)->next;    // advance the source pointer
+       newNode->next = *destRef;           // link the old dest off the new node
+       *destRef = newNode;                 // move dest to point to the new node
+   }
+
 
 int main()
 {
       struct node *test;
       struct node *dst=NULL;
 
-        test=buildonetwothree(1,2,3);
-       movenode(&dst,&test);
-        movenode(&dst,&test);
-           movenode(&dst,&test);
-        display(dst);
+      test=buildonetwothree(1,2,3);
+      movenode(&dst,&test);
+      movenode(&dst,&test);
+      movenode(&dst,&test);
+
+      display(dst);
     return 0;
 
 }
